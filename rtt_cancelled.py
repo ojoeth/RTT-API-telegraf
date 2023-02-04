a4a69b739fbe6e1dd0084d368bd312e2e43e4d1b
Joe H <joe@joeth.uk>
2023-02-04 10:04:20 +0000

    Add total count for trains so far that day

--- rtt_cancelled.py
+++ rtt_cancelled.py
@@ -8,29 +8,27 @@ password = sys.argv[2]
 station = sys.argv[3]
 session.auth = (user, password)
 
-def write_cancellations(cancelled, running, late, date, hour):
+def write_cancellations(cancelled, running, late, total, date, hour):
     con = sqlite3.connect("cancelcount.db")
     cur = con.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS servicecount(cancelled, running, late, date, hour, station)")
+    cur.execute("CREATE TABLE IF NOT EXISTS servicecount(cancelled, running, late, total, date, hour, station)")
 
-    datehourstation = (date, hour, station) # data to use for sql query
-    servicedata = (cancelled, running, late, date, hour, station) # data to use for sql query
+    servicedata = (cancelled, running, late, total, date, hour, station) # data to use for sql query
 
     # Check if data already exists for hour
-    exists = cur.execute("SELECT * FROM servicecount WHERE date=? AND hour=? AND station=?", datehourstation)
+    exists = cur.execute("SELECT * FROM servicecount WHERE date=? AND hour=? AND station=?", (date, hour, station))
     if exists.fetchone() is None:
-        cur.execute("INSERT INTO  servicecount VALUES (?, ?, ?, ?, ?, ?)", servicedata)
+        cur.execute("INSERT INTO  servicecount VALUES (?, ?, ?, ?, ?, ?, ?)", servicedata)
     else:
-        cur.execute("UPDATE servicecount SET cancelled=?, running=?, late=? WHERE date=? AND hour=? AND station=?", servicedata)
+        cur.execute("UPDATE servicecount SET cancelled=?, running=?, late=?, total=? WHERE date=? AND hour=? AND station=?", servicedata)
 
     con.commit()
     con.close()
 
-def fetch_db_data_day(date, hour):
+def fetch_db_data_day(date):
     con = sqlite3.connect("cancelcount.db")
     cur = con.cursor()
-    datehour = (date) # data to use for sql query
-    data = cur.execute("SELECT cancelled, running, late FROM servicecount WHERE date=? AND station=?", (date,station)).fetchall()
+    data = cur.execute("SELECT cancelled, running, late, total FROM servicecount WHERE date=? AND station=?", (date,station)).fetchall()
     con.close()
     return data
 
@@ -49,16 +47,18 @@ def fetchForHour(date_time):
     runningcount = 0
     cancelledcount = 0
     latecount = 0
+    totalcount = 0
     for service in lineup["services"]:
         if str(service["locationDetail"]["gbttBookedDeparture"])[0:2] == str(date_time.hour):
             if service["isPassenger"]:  
                 if service["locationDetail"]["displayAs"] == "CALL" or service["locationDetail"]["displayAs"] == "ORIGIN":
                     runningcount += 1
+                elif (int(service["locationDetail"]["realtimeDeparture"]) - int(service["locationDetail"]["gbttBookedDeparture"]) >= 2):
+                    latecount += 1
                 if service["locationDetail"]["displayAs"] == "CANCELLED_CALL":
                     cancelledcount += 1
-                if (int(service["locationDetail"]["realtimeDeparture"]) - int(service["locationDetail"]["gbttBookedDeparture"]) > 2):
-                    latecount += 1
-    return cancelledcount, runningcount, latecount
+                totalcount += 1
+    return cancelledcount, runningcount, latecount, totalcount
 
 
 now = datetime.datetime.now()
@@ -67,18 +67,20 @@ nexthour = now+datetime.timedelta(hours=1)
 datacurrenthour = fetchForHour(datetime.datetime(now.year, now.month, now.day, now.hour)) # fetch for current hour
 datanexthour = fetchForHour(datetime.datetime(nexthour.year, nexthour.month, nexthour.day, nexthour.hour)) # fetch for next hour
 
-write_cancellations(datacurrenthour[0], datacurrenthour[1], datacurrenthour[2], now.date(), now.hour)
-write_cancellations(datanexthour[0], datanexthour[1], datanexthour[2], nexthour.date(), nexthour.hour)
+write_cancellations(datacurrenthour[0], datacurrenthour[1], datacurrenthour[2], datacurrenthour[3], now.date(), now.hour)
+write_cancellations(datanexthour[0], datanexthour[1], datanexthour[2], datacurrenthour[3], nexthour.date(), nexthour.hour)
 
-daydata = fetch_db_data_day(now.date(), now.hour)
+daydata = fetch_db_data_day(now.date())
 
 cancelledday = 0
 runningday = 0
 lateday = 0
+totalday = 0
 
 for hour in daydata:
     cancelledday+=hour[0]
     runningday+=hour[1]
     lateday+=hour[2]
+    totalday+=hour[3]
 
-print("cancelledtoday="+ str(cancelledday), "ontimetoday=" + str(runningday), "latetoday=" + str(lateday))
+print("cancelledtoday="+ str(cancelledday), "ontimetoday=" + str(runningday), "latetoday=" + str(lateday), "totaltoday=" + str(totalday))
